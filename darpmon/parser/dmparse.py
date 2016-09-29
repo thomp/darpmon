5ae8debc33852bc46e5f968d3e7ba923a7702c2b
David Thompson <thompdump@gmail.com>
2016-09-28 22:58:54 -0700

    Support nicknames; support a ~/.dmparserc.json config file

--- darpmon/parser/dmparse.py
+++ darpmon/parser/dmparse.py
@@ -7,12 +7,29 @@
 
 import argparse
 import json
+import os
 import string
-import time
 import sys
+import time
 
-def human_readable_macs(macs):
-    t = string.Template('${MAC} on for ${minDelta} min (${on_at} to ${off_at})\n')
+def hrm_def_make_identifier(config):
+    use_nicks = True
+    if ('use_nicks' in config):
+        use_nicks = config['use_nicks']
+    if (use_nicks and ('nicks' in config)):
+        def make_identifier(mac):
+            if (mac in config['nicks']):
+                return config['nicks'][mac]
+            else:
+                return mac
+    else:
+        def make_identifier(mac):
+            return mac
+    return make_identifier
+
+def human_readable_macs(macs,config):
+    make_identifier = hrm_def_make_identifier(config)
+    t = string.Template('${identifier} on for ${minDelta} min (${on_at} to ${off_at}) [${IP}]\n')
     for mac in macs:
         record = macs[mac]
         first_time_raw = record['firstTime']
@@ -21,12 +38,18 @@ def human_readable_macs(macs):
         last_time = int(float(last_time_raw))
         sec_delta = last_time - first_time
         min_delta = sec_delta / 60
-        readable_first_time = time.asctime(time.gmtime(first_time))
-        readable_last_time = time.asctime(time.gmtime(last_time))
-        sys.stdout.write(t.substitute(MAC=mac,
+        # use gmtime if time_format is 'UTC'
+        # use localtime if time_format is 'localtime'
+        readable_first_time = time.asctime(time.localtime(first_time))
+        readable_last_time = time.asctime(time.localtime(last_time))
+        identifier = make_identifier(mac)
+        ipv4 = record['ipv4'] 
+        sys.stdout.write(t.substitute(identifier=identifier,
                                       minDelta=min_delta,
                                       on_at=readable_first_time,
-                                      off_at=readable_last_time ))
+                                      off_at=readable_last_time,
+                                      IP=ipv4
+        ))
 
 def parse_line(line,macs):
     """LINE is a string representing a single, raw line from the darpmon log."""
@@ -47,6 +70,14 @@ def main():
     parser.add_argument("input_files", help="one or more input (PDF) files", nargs="+", type=str)
     args = parser.parse_args()
     input_files = args.input_files
+    #
+    # deal with config file ~/.dmparserc.json
+    #
+    config_file_location = os.path.expanduser("~/.dmparserc.json")
+    f=open(config_file_location)
+    config_file_string = f.read()
+    f.close()
+    config = json.loads(config_file_string) 
     # keys are mac addresses
     # value has structure
     # { firstTime:             ,
@@ -64,7 +95,7 @@ def main():
     # - as JSON:
     #sys.stdout.write(str(macs))
     # - as human-readable summary
-    human_readable_macs(macs)
+    human_readable_macs(macs,config)
     
 if __name__ == "__main__":
     main()
